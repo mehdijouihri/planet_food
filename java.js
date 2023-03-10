3446be2d3037c74ac3def1de25b19c8324ad197b
mehdijouihri <100852016+mehdijouihri@users.noreply.github.com>
2023-03-10 12:28:43 +0100

    add script menu-burger

--- java.js
+++ java.js
@@ -1,1 +1,6 @@
+const menuHburger = document.querySelector(".menu-hburger")
+const navLinks = document.querySelector(".nav-links")
 
+menuHburger.addEventListener('click',()=> {
+    navLinks.classList.toggle('mobile-menu')
+})
